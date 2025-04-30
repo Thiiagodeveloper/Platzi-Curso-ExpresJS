5e7aadf23bec447e421e08832c13e6db4a549708
Santiago <santiagoloaizagarcia14.com>
2025-04-29 22:46:49 -0500

    Configuarcion de variables de entorno y gestion de versiones

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 3000
+
+const PORT = process.env.PORT || 3010
 
 app.get('/', (req, res) => {
     res.send(`
@@ -11,5 +13,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port', ${PORT}`)
+    console.log(`Server is running on port, http:localhost/${PORT}`)
 });
