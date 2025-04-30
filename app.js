612d780c7d0e7f8c767b60f54ab35a61572c6cc5
Santiago <santiagoloaizagarcia14.com>
2025-04-30 12:40:47 -0500

    Configuracion de Body Parser en Express para APIs REStful

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 
+const app = express()
+app.use(bodyParser.json())// Middleware to parse JSON bodies, entienda envios de JSON
+app.use(bodyParser.urlencoded({ extended: true })) // Middleware to parse URL-encoded bodies, entienda envios de formularios
 
 const PORT = process.env.PORT || 3010
 
@@ -10,6 +13,18 @@ app.get('/', (req, res) => {
         <h1>Welcome to the Express.js App v2</h1>
         <p>Use the following endpoints to test the app:</p>
         <p>Corre en el puerto: ${PORT}</p>`)
+})
+
+//Rutas
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id
+    res.send('User ID: ' + userId)
+})
+
+app.get('/search', (req, res) => {
+    const term = req.query.termino || 'No especified'
+    const category = req.query.categoria || 'Todas'
+    res.send(`Search term: ${term}, Category: ${category}`)
 })
 
 app.listen(PORT, () => {
