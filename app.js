446ef9e4f0d06964a0860b464cc52f28457ee78f
Santiago <santiagoloaizagarcia14.com>
2025-04-30 13:07:17 -0500

    Procesamiento de Formularios y JSON con Body Parser

--- app.js
+++ app.js
@@ -21,11 +21,44 @@ app.get('/user/:id', (req, res) => {
     res.send('User ID: ' + userId)
 })
 
+//Rutas con query params
 app.get('/search', (req, res) => {
-    const term = req.query.termino || 'No especified'
-    const category = req.query.categoria || 'Todas'
-    res.send(`Search term: ${term}, Category: ${category}`)
+    const terms = req.query.termino || "no especificado";
+    const category = req.query.categoria || "todas";
+
+    res.send(`<h2>Resultados de búsqueda</h2>
+              <p>Término: ${terms}</p>
+              <p>Categoría: ${category}</p>`);
+});
+
+app.post('/form', (req, res) => {
+    const name = req.body.nombre || "no especificado";
+    const emal = req.body.email || "no Proporcionado";
+    res.json({
+        message: "Formulario recibido",
+        data: {
+            name: name,
+            email: emal
+        }
+    })
 })
+
+app.post('/api/data', (req, res) => {
+    const data = req.body
+
+    if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({
+            error: "No data provided"
+        })
+    }
+
+    res.json({
+        message: "Data received",
+        data: data
+    })
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port, http:localhost/${PORT}`)
